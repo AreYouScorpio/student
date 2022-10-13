f10c8563db40bfb571fe68cbbe6591df7a804465
AreYouScorpio <akos.schweger@gmail.com>
2022-10-13 15:06:38 +0200

    updated student app project

---
+++ src/main/java/hu/webuni/student/StudentApplication.java
@@ -0,0 +1,45 @@
+package hu.webuni.student;
+
+import hu.webuni.student.service.AirportService;
+import hu.webuni.student.service.InitDbService;
+import hu.webuni.student.service.PriceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@RequiredArgsConstructor
+//@SpringBootApplication
+//ha security nélkül akarjuk futtatni:
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class StudentApplication implements CommandLineRunner{
+	@Autowired
+	PriceService priceService;
+
+	@Autowired
+	AirportService airportService;
+
+	@Autowired
+	InitDbService initDbService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		// airportService.createFlight();
+		System.out.println(priceService.getFinalPrice(200));
+		System.out.println(priceService.getFinalPrice(20000));
+
+//		initDbService.createUsersIfNeeded();
+
+		initDbService.addInitData();
+	}
+
+//teszt
+
+}
