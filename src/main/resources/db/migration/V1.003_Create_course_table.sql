25a445778b4508c095da303cd408256dd2b8e708
AreYouScorpio <akos.schweger@gmail.com>
2022-11-06 21:13:02 +0100

    flyway update

--- src/main/resources/db/migration/V1.003_Create_course_table.sql
+++
@@ -1,3 +0,0 @@
-create table course (id int8 not null, name varchar(255), primary key (id))
-create table course_students (courses_id int8 not null, students_id int8 not null, primary key (courses_id, students_id))
-create table course_teachers (course_id int8 not null, teachers_id int8 not null, primary key (course_id, teachers_id))
